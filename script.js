f566dad9cba1dd490198dee124cc68b6a14e6cb8
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 13:36:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+var num=1;
+
 document.getElementById('check_1').onclick = function() {check()};
 function check(){
-	var t=document.getElementById('title_1').value;
-	if (t=='問題1'){
+	if (num==1){
 		var x=document.getElementById('ans_1').value;
 		if (x==''){
 			document.getElementById('mess_1').innerHTML='你還沒作答！';}
@@ -11,7 +12,7 @@ function check(){
 		else{
 			document.getElementById('mess_1').innerHTML='答錯了QQ';}
 	}
-	else if (t=='問題2'){
+	else {
 		var x=document.getElementById('ans_1').value;
 		if (x==''){
 			document.getElementById('mess_1').innerHTML='你還沒作答！';}
@@ -25,16 +26,15 @@ function check(){
 
 document.getElementById('next').onclick = function() {change()};
 function change(){
-	var y=document.getElementById('title_1').value;
-	if (y=='問題1'){
+	if (num==1){
 		document.getElementById('title_1').innerHTML='問題2';
 		document.getElementById('question_1').innerHTML='請問下圖兩位哲學家是什麼關係？';
 		document.getElementById('img_1').setAttribute ('src','02.jpg')
-		document.getElementById('next').style.visibility = '';}
-	else if (y=='問題2'){
+		document.getElementById('next').style.visibility = 'hidden';
+		num=num+1;}
+	else {
 		document.getElementById('title_1').innerHTML='恭喜過關！';
 		document.getElementById('question_1').innerHTML='其實是沒有題目了呵呵';
-		document.getElementById('img_1').setAttribute ('src','03.jpg')}
-	else {
-		document.getElementById('mess_1').innerHTML='沒題目了啦還按';}
+		document.getElementById('next').style.visibility = 'hidden';
+		document.getElementById('check_1').style.visibility = 'hidden';}
 }
