aef9010d1546bc546e68039fe9cf908092b4e974
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 11:32:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function check(){
 		var x=document.getElementById('ans_1').value;
 		if(x=='') throw '你還沒作答！';
 		if(x=='雅典學院') throw '答案正確^^';
-		if(x!='雅典學院') throw '答錯了QQ';
+		if(x!='雅典學院' && x!='') throw '答錯了QQ';
 	}
 	catch(err){
 		var y=document.getElementById('mess_1');
