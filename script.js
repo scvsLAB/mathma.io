8ad629d43c29dc0bcc7448b6d1da146c2a34fe90
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 12:27:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.getElementById('show').onclick = function() {change()};
-
-function change(){
-	document.getElementById('show').innerHTML='粗體即現在範圍';
-}
-
 document.getElementById('check_1').onclick = function() {check()};
 
 function check(){
@@ -16,5 +10,17 @@ function check(){
 	}
 	else{
 		document.getElementById('mess_1').innerHTML='答錯了QQ';
+	}
+}
+
+var img = document.getElementById('img');
+
+document.getElementById('next').onclick = function() {change()};
+function change(){
+	var y=document.getElementById('mess_1').value;
+	if (y=='答案正確^^'){
+		document.getElementById('title').innerHTML='問題2';
+		document.getElementById('question').innerHTML='請問下圖兩位哲學家是什麼關係？';
+		document.getElementById('img').setAttribute ('src','02.jpg')
 	}
 }
