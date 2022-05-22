5e38c940e961613d870f75bfd5e5257e392e2375
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 16:17:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,21 +27,22 @@ function check(){
 document.getElementById('next').onclick = function() {change()};
 function change(){
 	if (num==1){
-		document.getElementById('title_1').innerHTML='問題2';
+		document.getElementById('title_1').innerHTML='<strong>問題2</strong>';
 		document.getElementById('question_1').innerHTML='請問下圖兩位哲學家是什麼關係？';
 		document.getElementById('img_1').setAttribute ('src','02.jpg')
 		document.getElementById('next').style.visibility = 'hidden';
+		document.getElementById('ans_1').innerHTML='';
 		document.getElementById('mess_1').innerHTML='你答對了嗎？';
 		num=num+1;}
 	else if(num==2){
-		document.getElementById('title_1').innerHTML='恭喜過關！';
+		document.getElementById('title_1').innerHTML='<strong>恭喜過關！</strong>';
 		document.getElementById('question_1').innerHTML='其實是沒有題目了呵呵';
 		document.getElementById('ans_1').style.visibility = 'hidden';
 		document.getElementById('check_1').style.visibility = 'hidden';
 		document.getElementById('mess_1').style.visibility = 'hidden';
 		num=num+1;}
 	else{
-		document.getElementById('title_1').innerHTML='是在哈囉';
+		document.getElementById('title_1').innerHTML='<strong>是在哈囉</strong>';
 		document.getElementById('question_1').innerHTML='就說沒有題目了還按';
 		document.getElementById('next').style.visibility = 'hidden';
 		document.getElementById('img_1').setAttribute ('src','03.jpg');}
