cce0266cf0cffac0ac582b37137037f241e3fdf6
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 11:28:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById("check_1").onclick = function() {check()};
+document.getElementById('check_1').onclick = function() {check()};
 
 function check(){
 	try{ 
 		var x=document.getElementById('ans_1').value;
-		if(x=='') throw '你還沒作答';
+		if(x=='') throw '你還沒作答！';
 		if(x=='雅典學院') throw '答案正確^^';
 		if(x!='雅典學院') throw '答錯了QQ';
 	}
 	catch(err){
-		var y=document.getElementById("mess_1");
+		var y=document.getElementById('mess_1');
 		y.innerHTML=err;
 	}
 }
