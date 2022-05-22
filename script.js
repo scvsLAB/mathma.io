0f3eeb8f823f9d64ad7dac7c01d1bf0427d7d404
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 12:43:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function check(){
 
 document.getElementById('next').onclick = function() {change()};
 function change(){
-	var y=document.getElementById('mess_1').value;
-	if (y=='答案正確^^'){
+	var y=document.getElementById('ans_1').value;
+	if (y=='雅典學院'){
 		document.getElementById('title').innerHTML='問題2';
 		document.getElementById('question').innerHTML='請問下圖兩位哲學家是什麼關係？';
 		document.getElementById('img').setAttribute ('src','02.jpg')
