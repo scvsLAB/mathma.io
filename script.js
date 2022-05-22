462a47d25ac631ca93d14aaf92934a9081e37b24
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2022-05-22 12:54:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById('check_1').onclick = function() {check()};
+document.getElementById('next').style.visibility = false;
 
+document.getElementById('check_1').onclick = function() {check()};
 function check(){
 	var x=document.getElementById('ans_1').value;
 	if (x==''){
@@ -7,6 +8,7 @@ function check(){
 	}
 	else if (x=='雅典學院'){
 		document.getElementById('mess_1').innerHTML='答案正確^^';
+		document.getElementById('next').style.visibility = true;
 	}
 	else{
 		document.getElementById('mess_1').innerHTML='答錯了QQ';
@@ -15,13 +17,7 @@ function check(){
 
 document.getElementById('next').onclick = function() {change()};
 function change(){
-	var y=document.getElementById('ans_1').value;
-	if (y=='雅典學院'){
-		document.getElementById('title').innerHTML='問題2';
-		document.getElementById('question').innerHTML='請問下圖兩位哲學家是什麼關係？';
-		document.getElementById('img').setAttribute ('src','02.jpg')
-	}
-	else {
-		
-	}
+	document.getElementById('title_1').innerHTML='問題2';
+	document.getElementById('question_1').innerHTML='請問下圖兩位哲學家是什麼關係？';
+	document.getElementById('img_1').setAttribute ('src','02.jpg')
 }
